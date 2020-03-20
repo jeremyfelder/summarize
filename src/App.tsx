825f0c8c207bf0b7772b52728bde14c3d66ba3c0
Jeremy Felder <jeremy.felder1@gmail.com>
2020-03-20 16:29:00 +0200

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { StatusBar } from 'react-native';
 import { OnboardingStack } from './navigation/OnboardingStack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 declare var global: {HermesInternal: null | {}};
 
 class App extends Component {
   render () {
     return (
-      <NavigationContainer>
-        <StatusBar backgroundColor="black" />
-        <OnboardingStack />
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <StatusBar backgroundColor="black" />
+          <OnboardingStack />
+        </NavigationContainer>
+      </SafeAreaProvider>
     )
   }
 };
