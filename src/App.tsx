4ef9358a83122a1d2aedcb6d926f6c0c2341ac54
Jeremy Felder <jeremy.felder1@gmail.com>
2020-06-12 11:40:08 +0300

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import MainTabNavigator from './navigation/MainTabNavigator';
+import { MainTabNavigator } from './navigation';
 
 function App () {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <StatusBar backgroundColor="black" />
+        <StatusBar backgroundColor="black"/>
         <MainTabNavigator />
       </NavigationContainer>
     </SafeAreaProvider>
